b0ce2c0d732c3d0c40d13f8ceacf6cf4c6a305f9
ioanachi <ioana_chichi@outlook.com>
2017-09-23 00:16:42 +0300

    Button that erases the submenu items

--- properMenuScroll(JQ position fixed)/src/js/script.js
+++ properMenuScroll(JQ position fixed)/src/js/script.js
@@ -1,31 +1,33 @@
 var $ = require("jquery");
 
-console.log($(document));
 
 $(document).ready(function() {
 
   var menu = $(".menu");
   var position = menu.offset();
   var menuTop = position.top;
-  console.log(menuTop);
 
 
   $(document).scroll(function() {
     var doc = $(document).scrollTop();
-    console.log(doc);
     if (doc >= 100) {
       menu.addClass("atTop");
     } else {
       menu.removeClass("atTop");
     }
-    console.log("test");
   })
   $(".menu a").click(function() {
     $(".menu a").removeClass("active");
     $(this).addClass("active");
   });
 
+/*
+-------
+// click pe liste
+-------
+*/
 
+//modificat calendarul cand se da click de o data
 
   //
   $(".table tr th").click(function() {
@@ -33,27 +35,44 @@ $(document).ready(function() {
     $(this).addClass("cell");
   });
 
+
+// modificat titlul cand se da click
+
   $(".mouse h3").click(function() {
     $(this).html("You clicked this title. Why did you do that?");
   });
 
+
+  //modificat tabelul cu submenu sa arate la al 2 lea click acelasi valoare
+// ia copii din lista adica li
   var copii = $(".lista").children();
-  // console.log(typeof copii);
+  // creaza un array gol
   var originale = [];
+  // baga in array valorile initiale din lista cu push
   $.each(copii, function(index, element) {
     var itemHTML = $(element).children().html();
     originale.push(itemHTML);
   });
-  console.log(originale);
+
+
+// modifica textul la primul click
   var slide = "This was clicked! Please click again";
-  $(".lista li a").click(function() {
+
+  // verif daca textul e modificat si daca e inl schimba in original
+  $(".lista li").click(function() {
     var index = $(this).index();
-    if ($(this).html() == slide) {
-      $(this).html(originale[index]);
+    var aHref = $(this).children();
+    console.log(aHref);
+    if (aHref.html() == slide) {
+      aHref.html(originale[index]);
     } else {
-      $(this).html(slide);
+      aHref.html(slide);
     }
   });
+
+
+
+
   // functie sageata go to top
   $(".goToTop").click(function() {
     $("html, body").animate({
@@ -74,9 +93,8 @@ $(document).ready(function() {
 
   // added a random class to the tag cloud
 
-  var cloud = ["small", "tiny", "large", "huge", "medium"]
+  var cloud = ["small", "tiny", "large", "huge", "medium"];
   var maxim = cloud.length - 1;
-  console.log(maxim);
 
   function getRandomArbitrary(min, max) {
     return Math.random() * (max - min) + min;
@@ -99,25 +117,32 @@ function nrRandom(min,max){
 }
   //dinamic add classes
   var whateverA = $('.whatever li a');
-  console.log(whateverA[0]);
   $.each(whateverA, function(key,value) {
     var varO = cloud[nrRandom(0,cloud.length-1)];
     $(value).addClass(varO);
-    console.log(value);
   });
 
 
   //function that delets the paragraph when the paragraph is clicked
 $("p").click(function(){
   $(this).remove()
+});
+//function that delets one of the paragraphs when the button is clicked
 
 
 
+var lis = $(".lista li");
+console.log(lis);
+
+var randLista = lis[0, lis.length-1];
+for(i = 0; i < $lis.length; i++) {
 
 
-})
+$("button").click(function(){
+randLista.remove();
+console.log("test");
 
-
-
+});
+});
 
 });
