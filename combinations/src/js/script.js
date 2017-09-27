196ccfd9ed230da13977c77aeb9050f50710930c
ioanachi <ioana_chichi@outlook.com>
2017-09-27 21:06:47 +0300

    not ready

---
+++ combinations/src/js/script.js
@@ -0,0 +1,51 @@
+var $ = require("jquery");
+$("document").ready(function(){
+
+$(".button").click(function(){
+var combin = $(".textB1").val();
+var cate = $(".textB2").val();
+var rest = combin - cate;
+var nN = 1;
+for (var i = combin; i < 0; i--) {
+  nN = i*nN;
+}
+console.log(nN);
+
+var rR = 1;
+for (var i = rest; i < 0; i--) {
+  rR = i*rR;
+}
+console.log(-----------------------------);
+console.log(rR);
+
+var nr = 1;
+for (var i = rest; i < 0; i--) {
+  nr = i*nr;
+}
+console.log(000000000000000000000000000000);
+console.log(nr);
+
+var reZult = nN/(nr*rR);
+$(".rezult").html(reZult);
+console.log(reZult);
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
